236b2bad0c48928faa01af45f673be439859313d
Oleh Mytsyk <oleh.mytsyk.25@kse.org.ua>
2026-06-28 10:56:21 +0300

    Добавив нові таблиці для розкладу, сервісів, спеціалізації та доктори, додавши проміжні таблиці для М:М

--- create_tables.sql
+++ create_tables.sql
@@ -13,11 +13,51 @@ drop table if exists appointments cascade;
 create table rooms(
 	id serial primary key,
 	--корпус
-	building varchar(200),
-	floor int,
+	building varchar(200) not null,
+	floor int not null,
 	--номер кабінету
-	number int,
-	display_name varchar(200),
+	number int not null,
+	display_name varchar(200) not null,
 	--тип (операційна і тд)
-	room_type varchar(200)
+	room_type varchar(200) not null
+);
+create table services(
+	id serial primary key,
+	--назва послуги (консультація, тд)
+	service_name varchar(200) not null,
+	description text not null,
+	price numeric(10,2) not null,
+	-- кількість часу, в хвилинах
+	duration int not null
+);
+--проміжки/слоти на запис
+create table work_shift_schedule(
+	id serial primary key,
+	start_time time not null,
+	end_time time not null
+);
+create table specialization (
+	id serial primary key,
+	name varchar(150) not null unique
+);
+create table doctors(
+	id serial primary key,
+	first_name varchar(50),
+	last_name varchar(50),
+	email varchar(70),
+	phone varchar(20),
+	active bool not null default true
+);
+
+create table doctors_specializations (
+	doctor_id int references doctors(id) on delete cascade,
+	specialization_id int references specialization(id) on delete cascade,
+	-- най дублюється скільки хоче, але комбінація НЕ МОЖЕ дублюватись.
+	primary key (doctor_id, specialization_id)
+);
+create table doctors_services (
+	doctor_id int references doctors(id) on delete cascade,
+	service_id int references services(id) on delete cascade,
+	-- най дублюється скільки хоче, але комбінація НЕ МОЖЕ дублюватись.
+	primary key (doctor_id, service_id)
 );
